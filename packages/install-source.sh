8887ad33fd6d9c49e7de228e0f63153fb4297e03
Andy A <uitm@sparrow.local>
2017-09-01 17:43:49 -0700

    Added cflags when installing on FreeBSD 10,11

--- packages/install-source.sh
+++ packages/install-source.sh
@@ -139,7 +139,7 @@ else
 fi
 
 printf " Please select your OS: \n\n"
-echo " 1. FreeBSD 10"
+echo " 1. FreeBSD 10, 11"
 echo " 2. SLES 12"
 echo " 3. Alpine 3.3"
 echo " 4. Fedora 24"
@@ -154,9 +154,9 @@ line=`echo $line | sed 's/^\(.\).*/\1/'`
 echo ""
 
 case $line in
-    # FreeBSD 10
+    # FreeBSD 10, 11
     1)
-        os="freebsd10"
+        os="freebsd"
 
         install_warn1
         check_packages
@@ -262,7 +262,11 @@ if [ "${os}" = "fedora24" -a "${arch64}" = "yes" ]; then
     echo 'install-purelib=$base/lib64/python' >> setup.cfg
 fi
 
-~/.local/bin/pip install --upgrade --target=amplify --no-compile -r packages/requirements
+if [ "${os}" = "freebsd" ]; then
+    opt='-std=c99'
+fi
+
+CFLAGS=${opt} ~/.local/bin/pip install --upgrade --target=amplify --no-compile -r packages/requirements
 
 if [ "${os}" = "fedora24" -a "${arch64}" = "yes" ]; then
     rm setup.cfg
@@ -281,7 +285,7 @@ ${sudo_cmd} chmod 644 ${agent_conf_file}
 detect_amplify_user
 
 if ! grep ${amplify_user} /etc/passwd >/dev/null 2>&1; then
-    if [ "${os}" = "freebsd10" ]; then
+    if [ "${os}" = "freebsd" ]; then
         ${sudo_cmd} pw user add ${amplify_user}
     else
         ${sudo_cmd} useradd ${amplify_user}
